7132134b22a553c5b2d42f0f9432b589149095d9
HanJiatong <hanjiatong@lianjia.com>
2015-10-09 01:48:17 +0800

    创建工程

---
+++ calendar/calendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calendar
+//
+//  Created by HanJiatong on 15/10/9.
+//  Copyright (c) 2015年 HanJiatong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ calendar/calendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  calendar
+//
+//  Created by HanJiatong on 15/10/9.
+//  Copyright (c) 2015年 HanJiatong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
