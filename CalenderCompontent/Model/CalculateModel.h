4da64819e5ef64706e9f054bf7874196163ad8ee
Jiatong Han <hanjiatong@lianjia.com>
2016-12-04 21:35:26 +0800

    添加pod依赖

---
+++ CalenderCompontent/Model/CalculateModel.h
@@ -0,0 +1,40 @@
+//
+//  CalculateModel.h
+//  calendar
+//
+//  Created by Jiatong Han on 16/11/26.
+//  Copyright © 2016年 HanJiatong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HJTDateModel;
+
+@interface CalculateModel : NSObject
+
+/** 
+    计算日期所在的indexPath
+ */
++ (NSIndexPath *)convertDateToIndexPath:(NSDate *)date;
+
+/** 
+    根据日期滚动到当月第一天
+ */
++ (NSIndexPath *)scrollToFirstRowByDate:(NSDate *)date;
+
+/**
+    根据indexPath计算当前日期
+ */
++ (HJTDateModel *)convertIndexPathToDate:(NSIndexPath *)indexPath;
+
+@end
+
+@interface HJTDateModel : NSObject
+
+/** indexPath中的日期*/
+@property (nonatomic, strong) NSDate *date;
+
+/** indexPath中的日期是不是下个月的日期*/
+@property (nonatomic, assign) BOOL isNextMonth;
+
+@end
---
+++ CalenderCompontent/category/NSDate+Addtion.h
@@ -0,0 +1,18 @@
+//
+//  NSDate+Addtion.h
+//  calendar
+//
+//  Created by HanJiatong on 15/10/9.
+//  Copyright (c) 2015年 HanJiatong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Addtion)
+
+/** 
+    阳历节日
+ */
+- (NSString *)holidayName;
+
+@end
